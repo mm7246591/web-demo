2736e98babdd3e622798bd67213b9559d4159464
Yan <84136753+mm7246591@users.noreply.github.com>
2023-03-28 11:25:33 +0800

    Update Loading.vue

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -6,7 +6,7 @@ const { getSettingLoding } = useSettingState();
 </script>
 <template>
     <div>
-        <Loading v-model:active="getSettingLoding" :can-cancel="false" :is-full-page="true" />
+        <Loading v-model:active="getSettingLoding" :can-cancel="false" :is-full-page="true" :lockScroll="true"/>
     </div>
 </template>
 
